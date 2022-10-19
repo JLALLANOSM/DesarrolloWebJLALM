9dfc2863808be05c3dabbb4fc48dbbbcb222c188
JLALLANOSM <joseluisllanosmendozajl70@gmail.com>
2022-10-19 17:28:52 -0400

    pila con php

---
+++ TAREAS/php/pila.php
@@ -0,0 +1,20 @@
+
+<?php
+    class Persona{
+        private $tope;
+        private $elementos[];
+        private $max;
+
+        public function __construct($max) {
+            $this->max = $max;
+        }
+    }
+
+    private function insertar($var) {
+        $tope++;
+        $this->elementos[$tope] = $var;
+    }
+
+    private function borrar($var) {
+
+?>
